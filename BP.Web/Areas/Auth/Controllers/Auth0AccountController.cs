3abb56f94dcfa2da1620dc9709aa091b0721b36d
Michael Brooks <michael@navkor.com>
2017-04-19 19:13:41 +0100

    Initiated Auth0 setup

---
+++ BP.Web/Areas/Auth/Controllers/Auth0AccountController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BP.Web.Areas.Auth.Controllers
+{
+    public class Auth0AccountController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> ExternalLoginCallback(string returnUrl)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+
+            var externalIdentity = await AuthenticationManager.GetExternalIdentityAsync(DefaultAuthenticationTypes.ExternalCookie);
+            if (externalIdentity == null)
+            {
+                throw new Exception("Could not get the external identity.  Please check your Auth0 configuration settings and ensure that you configured UseCookieAuthentication and UseExternalSignInCookie in the OWIN Startup class.  Also make sure you are not calling setting the callbackOnLocationHash option on the Javascript Login widget");
+            }
+
+            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = false }, CreateIdentity(externalIdentity));
+            return RedirectToLocal(returnUrl);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff(string returnUrl)
+        {
+            var appTypes = AuthenticationManager.GetAuthenticationTypes().Select(at => at.AuthenticationType).ToArray();
+            AuthenticationManager.SignOut(appTypes);
+
+            var absoluteReturnUrl = string.IsNullOrEmpty(returnUrl) ?
+                Url.Action("Index", "Home", new { area = "" }, Request.Url.Scheme) :
+                Url.IsLocalUrl(returnUrl) ?
+                    new Uri(Request.Url, returnUrl).AbsoluteUri : returnUrl;
+
+            return Redirect(
+                $"https://{ConfigurationManager.AppSettings["auth0:Domain"]}/v2/logout?returnTo={absoluteReturnUrl}"    
+            );
+        }
+
+        private static ClaimsIdentity CreateIdentity(ClaimsIdentity externalIdentity)
+        {
+            var identity = new ClaimsIdentity(externalIdentity.Claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+            identity.AddClaim(
+                new Claim(
+                    "http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                    "ASP.NET Identity",
+                    "http://www.w3.org/2001/XMLSchema#string"
+                )    
+            );
+            return identity;
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", new { Area = "" });
+            }
+        }
+    }
+}
